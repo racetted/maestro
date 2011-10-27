b91e5c22f61180094d8463f02eefa26b0d03ce2a
afsidor <dominic.racette@ec.gc.ca>
2011-10-27 14:11:54 +0000

    Version 1.0.8

--- config/config.mk
+++ config/config.mk
@@ -1,6 +1,6 @@
 # this file contains common config that is sourced by all makefiles
 WHO_I_AM=$(shell whoami)
-VERSION=1.0.7
+VERSION=1.0.8
 MACHINE=$(shell uname -s)
 ARCH=$(MACHINE)
 HARDWARE=$(shell uname -m)
